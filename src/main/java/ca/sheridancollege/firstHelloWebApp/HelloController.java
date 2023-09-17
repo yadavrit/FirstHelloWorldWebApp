f5db6e45162b7f13366d65a2edf504639d7b8450
Yadav Ritesh <yadavrit@sheridancollege.ca>
2023-09-17 11:39:31 -0400

    Firstcopy

--- src/main/java/ca/sheridancollege/firstHelloWebApp/HelloController.java
+++ src/main/java/ca/sheridancollege/firstHelloWebApp/HelloController.java
@@ -1,4 +1,16 @@
 package ca.sheridancollege.firstHelloWebApp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
+
+    @GetMapping("/index")
+    public String showRiteshyadavPage(Model model) {
+        return "index";
+    }
+
 }
+
